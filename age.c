c681f2e3b910d207c2aa9fb695d7fc3ad5a26a3f
Jazmin Gomez <gomjazmin12@gmail.com>
2025-04-01 14:49:57 +0000

    class 2

---
+++ age.c
@@ -0,0 +1,15 @@
+/*This program takes age input form the user 
+It stores it in the age variable
+And, print the value using printf()*/
+
+#include <stdio.h>
+
+int main (){
+//Create interger variable
+int age = 17;
+
+//Print the age variable
+printf( "Age: %d\n", age);
+
+return 0;
+}
