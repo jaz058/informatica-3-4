2b6a4012e683d161a5850aa3a70883d1ca223918
Jazmin Gomez <gomjazmin12@gmail.com>
2025-04-09 14:40:20 +0000

    Aaaaaaa

--- variables-operations.c
+++ variables-operations.c
@@ -5,10 +5,18 @@ int main(){
     int width = 15;
     int area = (length *width);
     int perimeter = (width * 2 + length *2);
+
 printf("area : %d Available\n", area);
 printf("perimeter : %d Available\n",perimeter);
+
 int  fahrenheit = 70;
 int celsius = (fahrenheit - 32) / 1.8;
-printf("%d")
+printf("\n%d°F = %d°C\n",fahrenheit, celsius);
+
+int time = 349;
+int hours = time/60;
+int minutes = time%60;
+printf("\n%d minutes is equal to %d minutes. %d\n", time, hours, minutes);
+return 0;
 
 }
