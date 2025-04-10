8655dd48f1b4e6e28edf88bbf81575c9365c0acc
Jazmin Gomez <gomjazmin12@gmail.com>
2025-04-10 16:29:26 +0000

    ++++++++

--- variables-operations.c
+++ variables-operations.c
@@ -4,26 +4,33 @@ int main(){
     int length;
     printf("Enter a rectangular length: ");
     scanf("%d", &length);
-    printf("Number = %d",length);
-    int width 
+    int width;
     printf("Enter a rectamgular width: ");
     scanf("%d", &width);
-    printf("Number = %d",width);
 
-    int area = (length *width);
+    int area = (length * width);
     int perimeter = (width * 2 + length *2);
 
-printf("area : %d Available\n", area);
-printf("perimeter : %d Available\n",perimeter);
+printf("area : %d\n", area);
+printf("perimeter : %d\n",perimeter);
 
-int  fahrenheit = 70;
+int  fahrenheit;
+scanf("%d", &fahrenheit);
 int celsius = (fahrenheit - 32) / 1.8;
 printf("\n%d°F = %d°C\n",fahrenheit, celsius);
 
-int time = 349;
+int time ;
+scanf("%d",&time);
 int hours = time/60;
 int minutes = time%60;
 printf("\n%d minutes is equal to %d minutes. %d\n", time, hours, minutes);
+float pi = 3.14;
+int radio;
+printf("Enter an integar for the radio of a circle:");
+scanf("%d",&radio);
+int diameter = radio * radio;
+float areac= diameter * pi;
+printf("the area of a circle that it's diameter is %d, will be: %2f", diameter, areac);
 
 
 return 0;
