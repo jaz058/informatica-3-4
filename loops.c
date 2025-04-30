7dfbbb8a2ecdb17f28e9b93e140e98007556d81c
Jazmin Gomez <gomjazmin12@gmail.com>
2025-04-30 14:49:27 +0000

    ****

---
+++ loops.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    // Example: Print numbers from 1 to 10
+    /*int i;
+
+for ( i = 1; i < 21;++i)
+{
+    printf("%d",i);
+}
+return 0;*/
+
+// Exercise 1
+int n;
+printf("Enter a positive number:");
+scanf("%d", &n);
+
+int i;
+int sum = 0;
+for (i = 1; i < n + 1 ; ++i){
+sum = sum + i;
+}
+printf("%d", sum);
+
+// Exercise 2
+int k;
+printf("Enter a positive number:");
+scanf("%d", &k);
+
+int l;
+int 
+}
