ab78f9b8394986425338a917d8baf0c99ce75c85
Jazmin Gomez <gomjazmin12@gmail.com>
2025-05-07 14:48:36 +0000

    lol

---
+++ do-intro.c
@@ -0,0 +1,10 @@
+//Print number from 1 to 5
+
+#include <stdio.h>
+
+int main (){
+   int i=1;
+   int n;
+   printf("Enter a number:");
+   scanf("%d",&n);
+}
--- m.c
+++ m.c
@@ -4,7 +4,7 @@ int main () {
     for(int row = 1; row < 6; row ++) {
     for ( int col = 1;col < 6; col++){
         int mul = row * col;
-        printf("%d/t", mul);
+        printf("%d\t", mul);
     }
     printf("\n");
     }
