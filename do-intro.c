670165578fe3ed4f509fe4f9084b41c26957111a
Jazmin Gomez <gomjazmin12@gmail.com>
2025-05-12 16:31:25 +0000

    Hola profe

--- do-intro.c
+++ do-intro.c
@@ -1,10 +1,19 @@
-//Print number from 1 to 5
-
 #include <stdio.h>
 
 int main (){
-   int i=1;
    int n;
-   printf("Enter a number:");
+   int count = 0;
+
+   printf("Enter a positive number:");
    scanf("%d",&n);
+
+   do{
+      n=n/10;
+      // n /=10
+
+      count++;
+   }
+   while(n!=0);
+
+   printf("Number of digits:%d\n",count);
 }
---
+++ exponents.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+//While loop exponent
+//int main(){
+//int base;
+  //  printf("Enter a base number: ");
+    //scanf("%d", &base);
+
+    //int exponent;
+    //printf("Enter the exponent : ");
+    //scanf("%d", &exponent);
+
+    //int i=1;
+//int result = base;
+    //do {result = base * result;
+       // i++;
+     //}
+     //while(i < exponent);
+
+     //printf("%d ^ %d\n", base, exponent, result);
+
+     //return 0;
+
+     //Library Functions
+     
+ {
+    float num, root;
+     printf("enter a number:");
+     scanf("%f", &num);
+
+root=sqrt (num);
+printf("squre root of %.2f=%.2f, num, root");
+}
+
+    
